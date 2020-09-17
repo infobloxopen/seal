6f1ba02314d98701a416658b244eb67a8f76b361
Phil Lu <wlu@infoblox.com>
2020-09-17 13:24:28 -0700

    GH-37: convert log to logrus

--- cmd/root.go
+++ cmd/root.go
@@ -16,11 +16,13 @@ limitations under the License.
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"github.com/spf13/cobra"
 	"os"
 
 	homedir "github.com/mitchellh/go-homedir"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
@@ -34,12 +36,13 @@ var rootCmd = &cobra.Command{
 authorization for their applications. SEAL provides an
 adaptable ecosystem that be targeted to a wide variety
 applications.`,
+	PersistentPreRunE: preRunE,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
-	if err := rootCmd.Execute(); err != nil {
+func Execute(ctx context.Context) {
+	if err := rootCmd.ExecuteContext(ctx); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
@@ -53,6 +56,12 @@ func init() {
 	// will be global for your application.
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.seal.yaml)")
+
+	rootCmd.PersistentFlags().StringP("logging.level", "l", "info", "log level of application (debug, info, warn, error, fatal, panic)")
+	viper.BindPFlag("logging.level", rootCmd.PersistentFlags().Lookup("logging.level"))
+
+	rootCmd.PersistentFlags().StringP("logging.format", "", "text", "log format of application (text, json)")
+	viper.BindPFlag("logging.format", rootCmd.PersistentFlags().Lookup("logging.format"))
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
@@ -83,4 +92,40 @@ func initConfig() {
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
+}
+
+// preRunE is executed before a subcommand is executed
+func preRunE(cmd *cobra.Command, args []string) error {
+	if err := setupLogger(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// setupLogger sets up the logger
+func setupLogger() error {
+	logger := logrus.StandardLogger()
+	if viper.GetString("logging.format") == "json" {
+		logger.SetFormatter(&logrus.JSONFormatter{PrettyPrint: false})
+	}
+
+	// Set the log level on the default logger based on command line flag
+	logLevels := map[string]logrus.Level{
+		"debug":   logrus.DebugLevel,
+		"info":    logrus.InfoLevel,
+		"warning": logrus.WarnLevel,
+		"warn":    logrus.WarnLevel,
+		"error":   logrus.ErrorLevel,
+		"fatal":   logrus.FatalLevel,
+		"panic":   logrus.PanicLevel,
+	}
+	spec := viper.GetString("logging.level")
+	if _, ok := logLevels[spec]; !ok {
+		logger.WithField("logging.level", "info").Warnf("overrode invalid log level: %s", spec)
+		spec = "info"
+	} else {
+		logger.WithField("logging.level", spec).Info("logging level")
+	}
+	logger.SetLevel(logLevels[spec])
+	return nil
 }
