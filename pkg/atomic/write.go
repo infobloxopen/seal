e9cfb1c78257be33719eca17ff0e541f23de42c3
Daniel García <daniel@danielgarcia.info>
2023-08-16 19:52:25 -0500

    write output file only if it changed

---
+++ pkg/atomic/write.go
@@ -0,0 +1,69 @@
+package atomic
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"io"
+	"os"
+	"path"
+	"reflect"
+
+	"github.com/sirupsen/logrus"
+)
+
+// WriteFile writes data to a file named by filename. If the contents of the file
+// are the same as the data, the file is not written.
+func WriteFile(filename string, data []byte, perm os.FileMode) error {
+
+	h := sha256.New()
+	h.Write(data)
+	newHash := h.Sum(nil)
+
+	// get the sha256 of the file contents if it exists
+	existingHash, _ := getSha256(filename)
+
+	if reflect.DeepEqual(newHash, existingHash) {
+		logrus.Debugf("file %s exists with same contents, skipping", filename)
+		return nil
+	}
+
+	// open temp file in same directory as target file
+	tempFile, err := os.CreateTemp(path.Dir(filename), path.Base(filename))
+	if err != nil {
+		return err
+	}
+	tname := tempFile.Name()
+	defer os.Remove(tname)
+
+	if _, err := io.Copy(tempFile, io.NopCloser(bytes.NewReader(data))); err != nil {
+		return err
+	}
+	if err := tempFile.Close(); err != nil {
+		return err
+	}
+	if err := os.Chmod(tname, perm); err != nil {
+		return err
+	}
+
+	if err := os.Rename(tname, filename); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// getSha256 returns the sha256 of the file contents if it exists
+func getSha256(filename string) ([]byte, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	h := sha256.New()
+	if _, err := io.Copy(h, f); err != nil {
+		return nil, err
+	}
+
+	return h.Sum(nil), nil
+}
